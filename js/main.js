76eb0dbe904fb496823f8c883412d408e289611d
Jessica <jesscara.993@gmail.com>
2022-12-06 16:31:03 +0100

    function for loop n email and push to array

--- js/main.js
+++ js/main.js
@@ -1,21 +1,33 @@
+/*
+  Decrizione:
+  generare 10 indirizzi email
+  e stamparli in pagina all’interno di una lista.
+*/
 const { createApp } = Vue;
 
 createApp({
   data () {
     return {
-
+      emailList: [],
     }
   },
 
   methods: {
-
+    generaMail(){
+      // genera 10 mail tramite api e ciclo for
+      for (let i = 0; i < 10; i++){
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((lista) => {
+        // console.log(lista.data.response);
+        // Inserisci le mail generate nell'array
+        this.emailList.push(lista.data.response);
+        })
+      }
+    }
   },
 
   mounted(){
-    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-    .then((lista) => {
-    // handle success
-    console.log(lista.data.response);
-  })
+    // mostra sul dom
+    this.generaMail();
   }
 }).mount('#app')
